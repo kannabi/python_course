b5e6249b4ce8c7c2be2609555abb8bd69b2dfcb2
kannabi <hekpoxuoypr@gmail.com>
2017-03-22 17:50:09 +0700

    Almost done

--- multiplicity3.4/Multiplicity.py
+++ multiplicity3.4/Multiplicity.py
@@ -3,14 +3,37 @@ class Multiplicity:
         if initial is None:
             initial = []
 
-        self.space_units = []
+        self.space_units = {}
         self.space = []
         self.n = 1
 
-        self.space_units.extend(initial)
+        for i in range(len(initial)):
+            self.space_units.update({i: initial[i]})
+        print(self.space_units)
 
     def next(self):
         buf = []
         self.n += 1
-        for i in range(self.n * len(self.space_units))
-            print("yo")
+
+        for i in range(len(self.space_units) ** self.n):
+            blank = i
+            while blank > 0:
+                buf.append(self.space_units.get(blank % len(self.space_units)))
+                blank //= len(self.space_units)
+
+            while len(buf) < self.n:
+                buf.append(self.space_units.get(0))
+
+            buf.reverse()
+            print(buf)
+            buf.clear()
+
+    def get_space(self):
+        return self.space
+
+
+test_mult = Multiplicity(['_', 'A'])
+for i in range(5):
+    test_mult.next()
+    print("================================================================================")
+# print(test_mult.get_space())
