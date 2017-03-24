905d7ffa19d8c6ebbcd68d6f9079c5ab7d3a6964
kannabi <hekpoxuoypr@gmail.com>
2017-03-24 15:44:19 +0700

    Done

--- multiplicity3.4/Multiplicity.py
+++ multiplicity3.4/Multiplicity.py
@@ -3,36 +3,22 @@ class Multiplicity:
         if initial is None:
             initial = []
 
-        self.space_units = {}
         self.space = []
-        self.n = 1
+        self.counter = 0
 
-        for i in range(len(initial)):
-            self.space_units.update({i: initial[i]})
+        self.space_units = tuple(initial)
+        self.space.append(self.space_units[0])
+
         print(self.space_units)
 
     def next(self):
-        buf = []
-        self.n += 1
+        self.counter += 1
+        blank = self.counter
+        self.space.clear()
 
-        for i in range(len(self.space_units) ** self.n):
-            blank = i
-            while blank > 0:
-                buf.insert(0, self.space_units.get(blank % len(self.space_units)))
-                blank //= len(self.space_units)
-
-            while len(buf) < self.n:
-                buf.insert(0, self.space_units.get(0))
-
-            print(buf)
-            buf.clear()
+        while blank > 0:
+            self.space.insert(0, self.space_units[blank % len(self.space_units)])
+            blank //= len(self.space_units)
 
     def get_space(self):
         return self.space
-
-
-test_mult = Multiplicity(['_', 'A'])
-for i in range(5):
-    test_mult.next()
-    print("================================================================================")
-# print(test_mult.get_space())
---
+++ multiplicity3.4/Multipliity_old.py
@@ -0,0 +1,38 @@
+class Multiplicity:
+    def __init__(self, initial=None):
+        if initial is None:
+            initial = []
+
+        self.space_units = {}
+        self.space = []
+        self.n = 1
+
+        for i in range(len(initial)):
+            self.space_units.update({i: initial[i]})
+        print(self.space_units)
+
+    def next(self):
+        buf = []
+        self.n += 1
+
+        for i in range(len(self.space_units) ** self.n):
+            blank = i
+            while blank > 0:
+                buf.insert(0, self.space_units.get(blank % len(self.space_units)))
+                blank //= len(self.space_units)
+
+            while len(buf) < self.n:
+                buf.insert(0, self.space_units.get(0))
+
+            print(buf)
+            buf.clear()
+
+    def get_space(self):
+        return self.space
+
+
+test_mult = Multiplicity(['_', 'A'])
+for i in range(5):
+    test_mult.next()
+    print("================================================================================")
+# print(test_mult.get_space())
