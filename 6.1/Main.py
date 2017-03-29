53e11047845e0ccb8b3284ccf8cf3b728fb32d1d
kannabi <hekpoxuoypr@gmail.com>
2017-03-29 22:21:24 +0700

    Half done

---
+++ 6.1/Main.py
@@ -0,0 +1,16 @@
+import random
+
+random.seed()
+
+with open("input.txt", 'r') as content_file:
+    content = content_file.read().split(" ")
+
+fout = open("output.txt", "w")
+
+for it in content:
+    it = list(it)
+    length = len(it)
+
+    for i in range(length - 1):
+        it.insert(length - 2, it.pop(random.randint(1, length - 2)))
+    fout.write("".join(it) + " ")
