537632f001e022725c5f0303dcd720bf08a49cc2
kannabi <hekpoxuoypr@gmail.com>
2017-03-30 21:32:22 +0700

    Added tests

--- 6.2/Get_url.py
+++ 6.2/Get_url.py
@@ -1,17 +1,22 @@
 from idea_test import Idea_test
 
-with open("input.txt", 'r') as content_file:
-    content = content_file.read().split(" ")
 
-length = len(content)
-for i in range(length):
-    content.extend(content.pop(i).split('\n'))
+def get_url(path_to_input_file, path_to_output_file):
+    with open(path_to_input_file, 'r') as content_file:
+        content = content_file.read().split(" ")
 
-fout = open("output.txt", "w")
+    length = len(content)
+    for i in range(length):
+        content.extend(content.pop(0).split('\n'))
 
-for it in content:
-    it = it.strip("\n")
-    if it[0:8] == "https://" or it[0:4] == "www.":
-        fout.write(it + "\n")
+    print(content)
+
+    fout = open(path_to_output_file, "w")
+
+    for it in content:
+        it = it.strip("\n")
+        if it[0:8] == "https://" or it[0:4] == "www.":
+            fout.write(it + "\n")
 
 Idea_test().test_simple_file()
+get_url("test_input.txt", "test_output.txt")
--- 6.2/Get_url_tests.py
+++ 6.2/Get_url_tests.py
@@ -1,14 +1,16 @@
 import unittest
 import random
 from string import ascii_letters
+from Get_url import get_url
+
 
 class Grt_url_tests(unittest.TestCase):
-
     def setUp(self):
-        self.url_set = ["ttps://pythonworld.ru/moduli/modul-random.html",
+        self.url_set = ("https://pythonworld.ru/moduli/modul-random.html",
                         "www.google.ru/search?q=read+from+file+python+3&oq=read+from+file+p&aqs=chrome.2.69i57j0l5.8071j0j7&sourceid=chrome&ie=UTF-8#newwindow=1&safe=off&q=python+3+string+equal&*",
-                        "https://vk.com/falko_s?z=photo-119287258_456239064%2Fwall23850766_7649"]
+                        "https://vk.com/falko_s?z=photo-119287258_456239064%2Fwall23850766_7649")
 
+        self.separators = " \n"
         self.input_file_name = "test_input.txt"
         self.output_file_name = "test_input.txt"
         f = open(self.input_file_name, "w")
@@ -20,13 +22,69 @@ class Grt_url_tests(unittest.TestCase):
     def test_simple_file(self):
         fin = open(self.input_file_name, "w")
 
+        symbols_set = ascii_letters + "\n"
         i = 0
         n = random.randint(3, 12)
-
         for j in range(n):
             if i < len(self.url_set) and random.randint(0, 1) == 1:
-                fin.write(self.url_set[i] + ' ')
+                fin.write(self.url_set[i] + random.choice(self.separators))
                 i += 1
-            fin.write(''.join([random.choice(ascii_letters) for _ in range(random.randint(1, 20))]))
+            fin.write(''.join([random.choice(symbols_set) for _ in range(random.randint(1, 20))])
+                      + random.choice(self.separators))
+
+        length = len(self.url_set)
+        if i < length - 2:
+            for k in range(i, length - 1):
+                fin.write(self.url_set[k] + random.choice(self.separators))
+
         fin.close()
 
+        get_url(self.input_file_name, self.output_file_name)
+
+        f = open(self.output_file_name, "r")
+
+        check = True
+        for line in f:
+            check = line.strip("\n") in self.url_set
+
+        f.close()
+        self.assertEqual(check, True)
+
+    def test_empty_file(self):
+        f = open(self.output_file_name, "w")
+        f.close()
+
+        get_url(self.input_file_name, self.output_file_name)
+
+        f = open(self.output_file_name, "r")
+        res = []
+        for line in f:
+            res.extend(line)
+        f.close()
+        self.assertEqual(len(res), 0)
+
+    def test_file_without_urls(self):
+        fin = open(self.input_file_name, "w")
+
+        symbols_set = ascii_letters + "\n"
+        n = random.randint(3, 12)
+        for j in range(n):
+            fin.write(''.join([random.choice(symbols_set) for _ in range(random.randint(1, 20))])
+                      + random.choice(self.separators))
+
+        fin.close()
+
+        get_url(self.input_file_name, self.output_file_name)
+
+        f = open(self.output_file_name, "r")
+
+        res = []
+        for i in f:
+            res.extend(i)
+
+        f.close()
+
+        self.assertEqual(len(res), 0)
+
+if __name__ == "__main__":
+        unittest.main()
