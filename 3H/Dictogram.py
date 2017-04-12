0cad74c4a5088292502d9b7f26774c386f828269
kannabi <hekpoxuoypr@gmail.com>
2017-04-12 11:02:27 +0700

    Reafactored. Created class Dictogram for storing probability data

---
+++ 3H/Dictogram.py
@@ -0,0 +1,54 @@
+class Dictogram:
+    def __init__(self):
+        self._keys = []
+        self._key_gist = []
+        self._word_gist = []
+
+    def put(self, key, token):
+        if not isinstance(key, str) or not isinstance(token, str):
+            raise ValueError
+
+        # if key not in self._keys:
+        self.add_key(key)
+
+        if token in self._word_gist[self._index(key)]:
+            self._word_gist[self._index(key)][token] += 1
+        else:
+            self._word_gist[self._index(key)].update({token: 1})
+
+    def add_key(self, key):
+        if key not in self._keys:
+            self._keys.append(key)
+            self._word_gist.append({})
+            self._key_gist.append(1)
+        else:
+            self._key_gist[self._index(key)] += 1
+
+    def get_probability(self, key, token):
+        if token in self._word_gist[self._index(key)]:
+            return self._word_gist[self._index(key)].get(token) / sum(self._word_gist[self._index(key)].values())
+        else:
+            return 0.0
+
+    def __getitem__(self, item):
+        pos = self._index(item)
+        num = sum(self._word_gist[pos].values())
+        return {token: self._word_gist[pos].get(token) / num for token in self._word_gist[pos].keys()}
+
+    def __str__(self):
+        answer = str()
+        for i in range(len(self._key_gist)):
+            answer += "  " + self._keys[i] + ": " + str(self._key_gist[i] / sum(self._key_gist)) + "\n"
+
+        for word in self._keys:
+            if not self._word_gist[self._index(word)]:
+                continue
+            gist = str()
+            num_tokens = sum(self._word_gist[self._index(word)].values())
+            for column in self._word_gist[self._index(word)].keys():
+                gist += "  " + column + ": " + str(self._word_gist[self._index(word)].get(column) / num_tokens) + "\n"
+            answer += word + ":\n" + gist
+        return answer
+
+    def _index(self, key):
+        return self._keys.index(key)
--- 3H/text_generator.py
+++ 3H/text_generator.py
@@ -1,17 +1,51 @@
 import re
+from Dictogram import Dictogram
+
+PATTERN = r'[a-zA-Zа-яА-Я\']+|[^a-zA-Z\d\n]{1}|[0-9]+'
+
+
+def get_raw_data(input_file):
+    with open(input_file) as fin:
+        return re.findall(PATTERN, fin.read())
 
 
 def get_words_set(input_file):
-    with open(input_file) as fin:
-        data = fin.read()
-    pattern = r'[a-zA-Zа-яА-Я]+|[^a-zA-Z\d\n]{1}|[0-9]+'
-    data = re.findall(pattern, data)
+    data = get_raw_data(input_file)
+    data = tuple(filter(lambda x: x != ' ', data))
     number_words = len(data)
     _corpus = {}
     for word in set(data):
         _corpus.update({word: data.count(word) / number_words})
     return _corpus
 
-corpus = get_words_set("input.txt")
-for w in corpus:
-    print(w, corpus.get(w))
+
+def get_probabilities(input_file, depth):
+    word_data = tuple(filter(lambda x: x != ' ', get_raw_data(input_file)))
+    probabilities = Dictogram()
+    try:
+        for i in range(len(word_data)):
+            cur_word = word_data[i]
+            for k in range(1, depth + 1):
+                prob_word = word_data[i + k]
+                probabilities.put(cur_word, prob_word)
+    except IndexError:
+        probabilities.add_key(prob_word)
+        return probabilities
+
+
+# corpus = get_words_set("input.txt")
+# for w in corpus:
+#     print("  " + w, corpus.get(w))
+
+res = get_probabilities("input.txt", 1)
+
+print(res)
+
+# d = Dictogram(tuple(set(filter(lambda x: x != ' ', get_raw_data("input.txt")))))
+#
+# d.put("test", "line")
+# d.put("test", "line")
+# d.put("test", "pisos pisosa")
+#
+# print(d)
+# print(d["test"])
