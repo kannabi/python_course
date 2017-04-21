2794651cf0231fff14e29f51ecab0f08b1d7e43e
kannabi <hekpoxuoypr@gmail.com>
2017-04-22 03:16:15 +0700

    refactored

--- 7.1/find_hitler.py
+++ 7.1/find_hitler.py
@@ -1,41 +1,36 @@
 import urllib.request
 import ssl
-import random
+import re
 
 import time
 
+pattern = re.compile(r"""<a\shref=\"/wiki/[\w]+_?[\w]+\"\stitle=\"[\w\s]+\">""", re.IGNORECASE)
+link_pattern = re.compile("""/wiki/[\w]+_[\w]+""", re.IGNORECASE)
 
-def get_articles(domen, start_address, target_address):
-    def proc_tag(item):
-        try:
-            if item[0] == 'a':
-                item = item.split(" ")
-                if (item[1][0:11] == "href=\"/wiki"
-                    and item[2][0:6] == "title="
-                    and item[1][0:16] != "href=\"/wiki/File"
-                    and item[1][0:22] != "href=\"/wiki/Wikipedia:"):
-                    return domen + "/wiki" + item[1][11: -1].replace(' ', '_')
-        except IndexError:
-            return
 
+def get_way(domen, start_address, target_address):
     ssl._create_default_https_context = ssl._create_unverified_context
 
-    articles = list()
-    articles.append(domen + start_address)
-    print(articles)
+    move_history = list()
+    finded = list()
+    cur_move = domen + start_address
     destiny = domen + target_address
-    random.seed()
-    while destiny not in articles:
-        cur_move = articles[random.randint(0, int(len(articles) / 2))]
+    while cur_move != destiny:
+        k = 0
+        while cur_move in move_history:
+            k += 1
+            try:
+                cur_move = domen + finded[k]
+            except IndexError:
+                print("We are in the endless recursion!")
         print(cur_move)
         with urllib.request.urlopen(cur_move) as fin:
-            data = str(fin.read()).split("<")
-            length = len(data)
-            for i in range(length):
-                data.extend(data.pop(0).split('>'))
-            articles = tuple(filter(lambda x: x is not None, tuple(map(proc_tag, data))))
+            finded = tuple(map(lambda x: link_pattern.search(x).group(0), link_pattern.findall(str(fin.read()))))
+            print(finded)
+        move_history.append(cur_move)
+        cur_move = domen + finded[0]
         time.sleep(1)
 
 
 wiki_addr = "https://en.wikipedia.org"
-get_articles(wiki_addr, "/wiki/Boris_Grebenshchikov", "/wiki/Philosophy")
+get_way(wiki_addr, "/wiki/Boris_Grebenshchikov", "/wiki/Philosophy")
