90c53ff81bf5077ad1b01acbac0975cfcafbcbfa
kannabi <hekpoxuoypr@gmail.com>
2017-05-05 01:57:41 +0700

    Done. Maybe

--- table3.1/Table.py
+++ table3.1/Table.py
@@ -1,40 +1,4 @@
-# n -- столбцы, m -- строки
 from functools import reduce
-
-
-class T:
-    def __init__(self, n, m):
-        self._table = [[str(j) for _ in range(m)] for j in range(n)]
-        self._n = n
-        self._m = m
-
-    def print(self):
-        for i in range(self._n):
-            print(' '.join(self._table[i]))
-
-    def add_lines(self, lines):
-        self._table.extend(lines)
-        self._n += len(lines)
-
-    def add_columns(self, columns):
-        for i in range(self._n):
-            self._table[i].extend(columns[i])
-        self._m += 1
-
-    def get(self, start, end):
-        return [[self._table[i][j] for i in range(self._m)] for j in range(start, end)]
-
-    def get_width(self):
-        return self._n
-
-    def get_height(self):
-        return self._m
-
-    def tail(self, n):
-        return self._table[n:]
-
-    def head(self, n):
-        return self._table[:n]
 
 
 class TableException(Exception):
@@ -54,9 +18,7 @@ class Table:
 
         self._table = [[i for i in j] for j in data]
         self._width = len(self._table)
-        print(self._width)
         self._height = len(self._table[0])
-        print(self._height)
 
         if not reduce(lambda a, x: a and len(x) == self._height, data, True):
             raise TableException('Wrong table size')
@@ -71,10 +33,10 @@ class Table:
         return self._height
 
     def tail(self, n):
-        return self._table[n:]
+        return Table(self._table[n:])
 
     def head(self, n):
-        return self._table[:n]
+        return Table(self._table[:n])
 
     def add_columns(self, adding_table):
         self._table.extend(adding_table.get_adding_table(range(adding_table.get_width())))
@@ -93,13 +55,8 @@ class Table:
     def get_rows(self, indexes):
         return [[self._table[i][j] for i in range(self._width)] for j in indexes]
 
-t1 = Table([['1', '2'], ['1', '3'], ['1', '2']])
+    def __getitem__(self, item):
+        return list(map(lambda x: x[item], self._table))
 
-t2 = Table([['13', '24'], ['11', '33'], ['22', '99']])
-
-# t1.add_columns(t2)
-t1.add_rows(t2)
-print(t1)
-
-
-# print(t1.get_rows([1]))
+    def extract_table(self, indexes):
+        return Table(self.get_columns(indexes))
